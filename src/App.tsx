a53a4b1f31c0d5cfa8fba6555432bb3e53654feb
Felippe Santana <felippefsantana02@gmail.com>
2023-06-13 14:54:51 -0300

    header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import './App.css';
+// import './App.css';
+
+// Components
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+// Styles CSS
+import styles from './App.module.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
+      <main className={styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
+
+      <Footer />
     </div>
   );
 }

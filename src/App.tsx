6028d4e8142d62e4beb67849cd70966e2556dc19
Felippe Santana <felippefsantana02@gmail.com>
2023-06-22 12:34:35 -0300

    update task

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,15 @@ function App() {
     setTaskToUpdate(task);
   }
 
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty }
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+    setTaskList(updatedItems);
+    hideOrShowModal(false);
+  }
+
   return (
     <div className="App">
       <Modal
@@ -48,6 +57,7 @@ function App() {
             btnText="Editar Tarefa"
             taskList={taskList}
             task={taskToUpdate}
+            handleUpdate={updateTask}
           />
         }
       />
